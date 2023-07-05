b326301448c40e19211ca05790aaddd83c2eea68
rusov63 <rusov63@yahoo.com>
2023-07-05 11:25:22 +0400

    add 22.2 Аутентификация

--- users/views.py
+++ users/views.py
@@ -12,6 +12,7 @@ from django.views.generic import UpdateView, CreateView, TemplateView
 from django.conf import settings
 
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 from users.forms import UserRegisterForm, UserProfileForm, UserForgotPasswordForm, UserSetNewPasswordForm
 from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView
 
@@ -35,7 +36,7 @@ class UserRegisterView(CreateView):
         user.is_active = False
         user.token = default_token_generator.make_token(user)
         activation_url = reverse_lazy(
-            'users:confirm_email', kwargs={'token': user.token}
+            'users:email_verified', kwargs={'token': user.token}
         )
         send_mail(
             subject='Подтверждение почты',
@@ -127,10 +128,11 @@ class UserForgotPasswordView(SuccessMessageMixin, PasswordResetView):
     """Представление по сбросу пароля по почте"""
     form_class = UserForgotPasswordForm
     template_name = 'users/user_password_reset.html'
-    success_url = reverse_lazy('users:login')
+    success_url = reverse_lazy('catalog:index')
     success_message = 'Письмо с инструкцией по восстановлению пароля отправлена на ваш email'
     subject_template_name = 'users/email/password_subject_reset_mail.txt'
     email_template_name = 'users/email/password_reset_mail.html'
+
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -138,10 +140,11 @@ class UserForgotPasswordView(SuccessMessageMixin, PasswordResetView):
         return context
 
 
+
 class UserPasswordResetConfirmView(SuccessMessageMixin, PasswordResetConfirmView):
     """Представление установки нового пароля"""
     form_class = UserSetNewPasswordForm
-    template_name = 'users/email/user_password_set_new.html'
+    template_name = 'users/user_password_set_new.html'
     success_url = reverse_lazy('users:login')
     success_message = 'Пароль успешно изменен. Можете авторизоваться на сайте.'
 
