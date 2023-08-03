7b49977bb67fd66bffc8fdc11e62381d935216c3
rusov63 <122866981+rusov63@users.noreply.github.com>
2023-08-03 22:13:23 +0400

    Update csu.py

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -3,12 +3,12 @@ from users.models import User
 import os
 
 
-PASSWORD = os.getenv('Pass_SQL')
+PASSWORD = os.getenv('PASSWORD')
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
-            email='rusov63@yahoo.com',
+            email=os.getenv('EMAIL_USER'),
             first_name='Admin',
             last_name='SuperUser',
             is_staff=True,
